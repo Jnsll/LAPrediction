2246bcd55886769b83b99560dfe93347b799670a
Jnsll <june.benvegnu-sallou@irisa.fr>
2020-11-05 21:27:42 +0100

    remove double import numpy + create poetry file setup

--- src/prediction/GBT_pred.py
+++ src/prediction/GBT_pred.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 import argparse
